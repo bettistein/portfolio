c2e5bccd06242bdadb8a93cafcc56069e24c2a61
Patrick Stapfer <ryyppy@users.noreply.github.com>
2018-02-18 16:28:12 +0100

    Add Next / Previous link to blog-post template

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,11 +1,47 @@
 import React from "react";
 import Helmet from "react-helmet";
 import get from "lodash/get";
+import find from "lodash/find";
+import last from "lodash/last";
+import first from "lodash/first";
+import Link from "gatsby-link";
+
+const getNextPage = (edges, slug) => {
+  const currentEdge = find(edges, edge => {
+    return edge.node.fields.slug === slug;
+  });
+  const { next } = currentEdge;
+
+  if (next == null) {
+    return first(edges).node.fields.slug;
+  }
+
+  return next.fields.slug;
+};
+
+const getPrevPage = (edges, slug) => {
+  const currentEdge = find(edges, edge => {
+    return edge.node.fields.slug === slug;
+  });
+  const { previous } = currentEdge;
+
+  if (previous == null) {
+    return last(edges).node.fields.slug;
+  }
+
+  return previous.fields.slug;
+};
 
 class BlogPostTemplate extends React.Component {
   render() {
-    const post = this.props.data.markdownRemark;
+    const post = this.props.data.currentBlog;
+    const allBlogEdges = this.props.data.allBlogs.edges;
+    const slug = get(this.props, "data.currentBlog.fields.slug");
+
     const siteTitle = get(this.props, "data.site.siteMetadata.title");
+
+    const nextPage = getNextPage(allBlogEdges, slug);
+    const prevPage = getPrevPage(allBlogEdges, slug);
 
     // defines the blogpost
     return (
@@ -20,16 +56,17 @@ class BlogPostTemplate extends React.Component {
         <h1>{post.frontmatter.title}</h1>
         <p
           style={{
-            display: "block",
+            display: "block"
           }}
         >
           {post.frontmatter.date}
         </p>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
-        <hr
-          style={{
-          }}
-        />
+        <hr style={{}} />
+        <div>
+          <Link to={prevPage}> Previous </Link>
+          <Link to={nextPage}> Next </Link>
+        </div>
       </div>
     );
   }
@@ -39,18 +76,40 @@ export default BlogPostTemplate;
 
 export const pageQuery = graphql`
   query BlogPostBySlug($slug: String!) {
+    allBlogs: allMarkdownRemark {
+      edges {
+        previous {
+          fields {
+            slug
+          }
+        }
+        node {
+          fields {
+            slug
+          }
+        }
+        next {
+          fields {
+            slug
+          }
+        }
+      }
+    }
     site {
       siteMetadata {
         title
         author
       }
     }
-    markdownRemark(fields: { slug: { eq: $slug } }) {
+    currentBlog: markdownRemark(fields: { slug: { eq: $slug } }) {
       id
       html
       frontmatter {
         title
         date(formatString: "MMMM DD, YYYY")
+      }
+      fields {
+        slug
       }
     }
   }
